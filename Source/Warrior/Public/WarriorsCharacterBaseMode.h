5ecebc038b091dcfda84c784457ff7ba1d66c31b
leohujiang <leohujiang@tencent.com>
2026-09-07 19:23:37 +0800

    Initial commit: Warrior UE5.4 project

---
+++ Source/Warrior/Public/WarriorsCharacterBaseMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarriorsCharacterBaseMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorsCharacterBaseMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
